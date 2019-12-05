27813a1071bcede4e6ba00642720c901a9d51a1c
dantegg <luroufan327@hotmail.com>
2019-12-05 10:14:37 +0800

    update

---
+++ src/main/java/com/coder/server/HelloServerHandler.java
@@ -0,0 +1,28 @@
+package com.coder.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+
+public class HelloServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            ByteBuf in = (ByteBuf) msg;
+            System.out.println(in.toString(CharsetUtil.UTF_8));
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
