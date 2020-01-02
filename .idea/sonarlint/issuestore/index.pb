2ae8f070f45908f23fb51184c3d4e5e2cdee622f
dantegg <luroufan327@hotmail.com>
2020-01-02 14:12:00 +0800

    update

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+f
+6src/main/java/com/coder/server/HelloServerHandler.java,7\c\7c02539cb34f471eb078e8bf06b36b57b65862b2
--- src/main/java/com/coder/server/HelloServerHandler.java
+++ src/main/java/com/coder/server/HelloServerHandler.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
+import java.io.UnsupportedEncodingException;
+
 public class HelloServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -25,4 +27,30 @@ public class HelloServerHandler extends ChannelInboundHandlerAdapter {
         ctx.close();
     }
 
+
+
+
+    public static String getOct(String s) throws UnsupportedEncodingException
+    {
+        String[] as = s.split("\\\\");
+        byte[] arr = new byte[as.length - 1];
+        for (int i = 1; i < as.length; i++)
+        {
+            int sum = 0;
+            int base = 64;
+            for (char c : as[i].toCharArray())
+            {
+                sum += base * ((int)c - '0');
+                base /= 8;
+            }
+            if (sum >= 128) sum = sum - 256;
+            arr[i - 1] = (byte)sum;
+        }
+        return new String(arr,"gb2312"); //如果还有乱码，这里编码方式你可以修改下，比如试试看unicode gbk等等
+    }
+
+    public static void main(String[] args) throws java.io.UnsupportedEncodingException {
+        String s = getOct("\350\277\345\233\236");
+        System.out.println("end is " + s);
+    }
 }
